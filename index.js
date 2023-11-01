33e4a93ee5a2bc449e885a5d1992c117b2481bac
user.email <developerjenzii@gmail.com>
2023-11-01 16:32:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(data, leftChild = null, rightChild = null){
+    this.data = data;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+  }
+}
+
+class Tree {
+  constructor(arr){
+    this.arr = arr = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.buildTree(this.arr, 0, this.arr.length - 1);
+  }
+
+  buildTree(arr, start, end) {
+    if (start > end) return null;
+    const mid = Math.floor((start + end) / 2);
+    const node = new Node(arr[mid]);
+
+    node.left = this.buildTree(arr, start, mid - 1);
+    node.right = this.buildTree(arr, mid + 1, end);
+    return node;
+  }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let arrTree = [1,2,3,4,5];
+let tree = new Tree([3,4,7,4,7,9,10,11]);
+
+prettyPrint(tree.root);
